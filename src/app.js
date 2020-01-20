59c0ab57aec923bba8b7a60f56673fd5a9bd5b84
joshua hahn <tailrin.jh@gmail.com>
2020-01-20 12:43:09 -0700

    setup SMS message via twilio library

--- src/app.js
+++ src/app.js
@@ -3,9 +3,21 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+const twilio = require('twilio');
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
 const NODE_ENV = process.env.NODE_ENV
+const client = new twilio(accountSid, authToken);
 
 const app = express()
+
+client.messages
+  .create({
+     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+     from: '+19045670593',
+     to: '+19705396535'
+   })
+  .then(message => console.log(message.sid));
 
 const morganOption = (NODE_ENV === 'production')
 	? 'tiny'
